60cab6dd4bfda42eddf03c6c4c3b5209c841b890
Arthur Martins <arthur.apxgp@gmail.com>
2026-04-09 10:43:49 -0300

    envio de arquivos

---
+++ Experimento 1/script.js
@@ -0,0 +1,12 @@
+// JavaScript para interatividade
+console.log("Página carregada com sucesso!");
+
+// Exemplo de função simples
+function saudar(nome) {
+ alert("Olá, " + nome + "!");
+}
+
+// Chamar função ao carregar a página
+window.addEventListener("load", function() {
+ console.log("DOM completamente carregado");
+});
